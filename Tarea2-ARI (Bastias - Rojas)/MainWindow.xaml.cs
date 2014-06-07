b27498990c81b75a0aa2f64b1c3c7cf39ce46fd7
asrojas <guitmet@gmail.com>
2014-06-07 18:35:13 -0400

    hashing lineal 1.0

--- Tarea2-ARI (Bastias - Rojas)/MainWindow.xaml.cs
+++ Tarea2-ARI (Bastias - Rojas)/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 
 namespace Tarea2_ARI__Bastias___Rojas_
 {
@@ -31,33 +31,114 @@ namespace Tarea2_ARI__Bastias___Rojas_
             this.Close();
         }
 
+        public static int Contador(string RUTA){
+
+
+            string fic = RUTA;            
+            System.IO.StreamReader sr = new System.IO.StreamReader(fic);  //carga el fichero en la variable sr para poder manipularlo
+
+            int count = 0;
+            string lineas;
+            
+            // recorre las lineas del archivo productos.txt
+            while ((lineas = sr.ReadLine()) != null)
+            {
+                count++;
+
+            }
+            // fin recorrido de lineas del archivo productos.txt
+
+
+            sr.Close();
+            Console.WriteLine(count);
+
+            return count;
+        }
+
         private void btnInicializar_Click(object sender, RoutedEventArgs e)
         {
 
-            //Lee archivo 
+            const string fic = @"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\productos.txt";  //lee el archivo productos.txt               
 
-            const string fic = @"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\productos.txt";
+            System.IO.StreamReader sr = new System.IO.StreamReader(fic);  //carga el fichero en la variable sr para poder manipularlo
+
+            // inicializacion de variables
+            int i = 0;
             string texto;
+            int ocurrencia = 0;
+            int valor;
+            int j=0;
+            string binary;
+            string sub_string;
+            int capacidad = 3;
+            // fin inicializacion de variables
 
-            System.IO.StreamReader sr = new System.IO.StreamReader(fic);
-            texto = sr.ReadToEnd();
+
+            //Console.WriteLine("cantidad de lineas - " + Contador(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\productos.txt"));
+
+
+           // System.IO.StreamWriter file;
+
+            FileInfo f;
+            StreamWriter writer;
+
+            // recorre las lineas del archivo productos.txt
+            while ((texto = sr.ReadLine()) != null)
+            {
+ 
+                ocurrencia = texto.IndexOf("&"); // obtiene la primera ocurrencia de '&'
+                sub_string = texto.Substring(0,ocurrencia); // obtiene el sub string hasta la primera ocurrencia de '&'
+                valor= Convert.ToInt32(sub_string); // convierte el numero string a numero int
+                binary = Convert.ToString(valor,2); // convierte el entero a binario
+
+                    if (System.IO.File.Exists(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt"))
+                    {
+
+          
+                        if (Contador(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado"+j+".txt") < capacidad)
+                        {
+                            
+                            f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
+            
+                            writer = f.AppendText();
+                            writer.WriteLine(texto);
+                            writer.Close();
+
+                        }
+                           else
+                           {
+
+                               f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
+                               
+
+
+                               writer = f.AppendText();
+                               writer.WriteLine(texto);
+                               writer.Close();
+
+                               j++;
+                           }
+  
+                    }
+                    else
+                    {
+                        f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
+                   
+
+                        writer = f.AppendText();
+                        writer.WriteLine(texto);
+                        writer.Close();
+                        
+                    }
+
+
+                //Console.WriteLine(sub_string+" - "+binary+" - "+binary.Length);
+            } 
+            // fin recorrido de lineas del archivo productos.txt
+
+
             sr.Close();
 
-            Console.WriteLine(texto);
-            Console.WriteLine(texto);
-            Console.Read();
-
-
-            //Escribe archivo
-
-            // Compose a string that consists of three lines.
-            string lines = texto;
-
-            // Write the string to a file.
-            System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\resultado.txt");
-            file.WriteLine(lines);
-
-            file.Close();
 
 
         }

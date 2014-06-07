e613f88fb4077f79c0b152441238b6ecd55c01da
asrojas <guitmet@gmail.com>
2014-06-07 18:40:48 -0400

    hashing lineal 1.2

--- Tarea2-ARI (Bastias - Rojas)/MainWindow.xaml.cs
+++ Tarea2-ARI (Bastias - Rojas)/MainWindow.xaml.cs
@@ -62,17 +62,12 @@ namespace Tarea2_ARI__Bastias___Rojas_
 
             System.IO.StreamReader sr = new System.IO.StreamReader(fic);  //carga el fichero en la variable sr para poder manipularlo
 
-<<<<<<< HEAD
             // inicializacion de variables
             int i = 0;
-=======
-            /*
-            const string fic = @"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\productos.txt";
->>>>>>> e6021d6e94c375e3fdcb7d735e0d81ba2c8a8d1e
             string texto;
             int ocurrencia = 0;
             int valor;
-            int j=0;
+            int j = 0;
             string binary;
             string sub_string;
             int capacidad = 3;
@@ -82,7 +77,7 @@ namespace Tarea2_ARI__Bastias___Rojas_
             //Console.WriteLine("cantidad de lineas - " + Contador(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\productos.txt"));
 
 
-           // System.IO.StreamWriter file;
+            // System.IO.StreamWriter file;
 
             FileInfo f;
             StreamWriter writer;
@@ -90,62 +85,59 @@ namespace Tarea2_ARI__Bastias___Rojas_
             // recorre las lineas del archivo productos.txt
             while ((texto = sr.ReadLine()) != null)
             {
- 
+
                 ocurrencia = texto.IndexOf("&"); // obtiene la primera ocurrencia de '&'
-                sub_string = texto.Substring(0,ocurrencia); // obtiene el sub string hasta la primera ocurrencia de '&'
-                valor= Convert.ToInt32(sub_string); // convierte el numero string a numero int
-                binary = Convert.ToString(valor,2); // convierte el entero a binario
+                sub_string = texto.Substring(0, ocurrencia); // obtiene el sub string hasta la primera ocurrencia de '&'
+                valor = Convert.ToInt32(sub_string); // convierte el numero string a numero int
+                binary = Convert.ToString(valor, 2); // convierte el entero a binario
 
-                    if (System.IO.File.Exists(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt"))
+                if (System.IO.File.Exists(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt"))
+                {
+
+
+                    if (Contador(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt") < capacidad)
                     {
 
-          
-                        if (Contador(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado"+j+".txt") < capacidad)
-                        {
-                            
-                            f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
-            
-                            writer = f.AppendText();
-                            writer.WriteLine(texto);
-                            writer.Close();
-
-                        }
-                           else
-                           {
-
-                               f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
-                               
-
-
-                               writer = f.AppendText();
-                               writer.WriteLine(texto);
-                               writer.Close();
-
-                               j++;
-                           }
-  
-                    }
-                    else
-                    {
                         f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
-                   
 
                         writer = f.AppendText();
                         writer.WriteLine(texto);
                         writer.Close();
-                        
+
                     }
+                    else
+                    {
+
+                        f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
+
+
+
+                        writer = f.AppendText();
+                        writer.WriteLine(texto);
+                        writer.Close();
+
+                        j++;
+                    }
+
+                }
+                else
+                {
+                    f = new FileInfo(@"C:\Users\casa\Desktop\universidad\2014-1 sem\ari\tarea2\t_anibal\tarea2_ari\Tarea2-ARI (Bastias - Rojas)\assets\resultado" + j + ".txt");
+
+
+                    writer = f.AppendText();
+                    writer.WriteLine(texto);
+                    writer.Close();
+
+                }
 
 
                 //Console.WriteLine(sub_string+" - "+binary+" - "+binary.Length);
-            } 
+            }
             // fin recorrido de lineas del archivo productos.txt
 
 
             sr.Close();
-
-
-            */
         }
 
         private void btnVentas_Click(object sender, RoutedEventArgs e)
